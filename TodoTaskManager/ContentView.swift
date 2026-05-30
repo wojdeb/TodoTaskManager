d6310125589b0d47fe3c66c1ac637cab58ca17c5
wojdeb <wojciech.debski96@gmail.com>
2026-05-30 18:22:50 +0200

    Initial Commit

---
+++ TodoTaskManager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TodoTaskManager
+//
+//  Created by Wojtek on 30/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TodoTaskManager/TodoTaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoTaskManagerApp.swift
+//  TodoTaskManager
+//
+//  Created by Wojtek on 30/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct TodoTaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
